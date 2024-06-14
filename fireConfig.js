478296426fce54b110ee6c784b66bc14ef2965c7
aaronbusti <144447938+aaronbusti@users.noreply.github.com>
2024-06-14 22:29:40 +0800

    Add files via upload

---
+++ fireConfig.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyB8CjCn2WG9ijKhnGVN-Y7ful_zXdx5lss",
+  authDomain: "crud-4c7bd.firebaseapp.com",
+  databaseURL: "https://crud-4c7bd-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "crud-4c7bd",
+  storageBucket: "crud-4c7bd.appspot.com",
+  messagingSenderId: "55889056302",
+  appId: "1:55889056302:web:c03debb45b64ac817505bf"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
